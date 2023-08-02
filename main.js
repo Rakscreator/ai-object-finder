f9a58f4edbb09e3bd2704bc694c45c6340bbab2f
RM <90820664+Rakscreator@users.noreply.github.com>
2023-08-02 21:23:49 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,27 +2,27 @@ Status = "";
 objects = [];
 
 function setup(){
-    canvas = createCanvas(300,290);
-    canvas.position(480,250);
+     canvas = createCanvas(650,500);
+    canvas.center();
     video = createCapture(VIDEO);
-    video.size(300,290);
+    video.size(650,500);
     video.hide();
 }
 function start(){
     object_Detector = ml5.objectDetector('cocossd',modelLoaded);
-    document.getElementById("status").innerHTML = "Status: Detecting Object";
-    input_text = document.getElementById("input_id").value;
+    document.getElementById("Mstats").innerHTML = "Status: Detecting Object";
+    input_text = document.getElementById("input").value;
 }
 function modelLoaded(){
     console.log("Model_Loaded");
     Status = true;
 }
 function draw(){
-    image(video,0,0,300,290);
+    image(video,0,0,650,500);
     if(Status != ""){
         object_Detector.detect(video, gotResults);
         for(i = 0;i < objects.length;i++){
-            document.getElementById("status").innerHTML = "Status : Object Detected";
+            document.getElementById("Mstats").innerHTML = "Status : Object Detected";
             console.log(objects.length);
             fill("#ff0000");
             percent = floor(objects[i].confidence * 100);
@@ -34,13 +34,17 @@ function draw(){
             if(objects[i].label == input_text){
                 video.stop();
                 object_Detector.detect(gotResults);
-                document.getElementById("object_found").innerHTML = input_text+" Found";
+                document.getElementById("Ostats").innerHTML = input_text+" Found";
                 var synth = window.speechSynthesis;
                 var utterThis = new SpeechSynthesisUtterance(input_text + "Found");
+                setTimeout(function(){
                 synth.speak(utterThis);
+                }, 2000); 
+                synth.cancel();
+                
             }
             else{
-                document.getElementById("object_found").innerHTML = input_text + " Not Found";
+                document.getElementById("Ostats").innerHTML = input_text + " Not Found";
             }
         }
     }
